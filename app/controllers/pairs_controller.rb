ab994657813ddb7c6e6f9c738a87d3458e341c85
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-27 13:56:19 +0300

    Add create method for pair controller

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -2,4 +2,23 @@ class PairsController < ApplicationController
   def display
     @pairs = Pair.all
   end
+
+  def manage
+    @currencies = Currency.all
+  end
+
+  def create
+    @pair = Pair.new(pair_params)
+    if @pair.save
+      flash[:notice] = 'Pair created successfully.'
+      redirect_to action: 'manage'
+    else
+      flash[:notice] = 'Pair creation failed.'
+      redirect_to action: 'manage'
+    end
+  end
+
+  def pair_params
+    params.require(:pair).permit(:currency1_id, :currency2_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pairs#display'
+  get 'pairs/manage' => 'pairs#manage'
+  post 'pairs/create' => 'pairs#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
