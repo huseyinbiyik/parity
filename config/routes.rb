867d8fe1f2af20fabac244870e177d883f41badf
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-27 22:05:41 +0300

    Add delete view for deleting currency pairs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pairs#display'
   get 'pairs/manage' => 'pairs#manage'
   post 'pairs/create' => 'pairs#create'
+  delete 'pairs/destroy/:id' => 'pairs#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
