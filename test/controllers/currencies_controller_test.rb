3a8a0fa64de4b8a7d89091d6f41e48f1a44f6908
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-28 11:20:19 +0300

    Remove minitest and add rspec

--- test/controllers/currencies_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CurrenciesControllerTest < ActionDispatch::IntegrationTest
-  test "should get display" do
-    get currencies_display_url
-    assert_response :success
-  end
-end
