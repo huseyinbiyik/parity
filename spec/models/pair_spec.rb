2120f518ee10411b186b07e933303bf5382eb191
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-28 11:21:46 +0300

    Add request specs for display and manage pages

---
+++ spec/models/pair_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Display Page', type: :request do
+  describe 'GET /pairs' do
+    it 'displays a list of pairs' do
+      get '/'
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'Manage Page' do
+    it 'displays a list of pairs' do
+      get '/pairs/manage'
+      expect(response).to have_http_status(:success)
+    end
+    it 'creates a new pair' do
+      post '/pairs/create', params: { pair: { currency1_id: 1, currency2_id: 2 } }
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+end
